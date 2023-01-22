13834d2fcab08b8b79ffcab016f5e54f874131ed
Satyam-Satyarthi <107866501+Satyam-Satyarthi@users.noreply.github.com>
2023-01-22 21:28:02 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,31 @@
+import cv2 as cv
+import numpy as np
+from cv2 import VideoCapture
+from cv2 import waitKey
+
+camera = VideoCapture(0)
+
+while True:
+    _, frame = camera.read()
+
+    #cv.imshow('Camera',frame)
+
+    laplacian = cv.Laplacian(frame , cv.CV_64F)
+    #laplacian = np.unit8(laplacian)
+    # cv.imshow('Laplacian',laplacian)
+
+    edges = cv.Canny(frame , 200 , 200)
+    cv.imshow('Canny', edges)
+
+    frame = cv.cvtColor(frame,cv.COLOR_BGR2GRAY).astype(float)
+    edge_x = cv.Sobel(frame , cv.CV_64F,1,0,ksize=3)
+    edge_y = cv.Sobel(frame , cv.CV_64F,0,1,ksize=3)    
+    edge = np.sqrt(edge_x**2 + edge_y**2) 
+    cv.imshow('Sobel' , edge)
+
+    if cv.waitKey(5) == ord('x'):
+        break
+
+
+camera.release()
+cv.destroyAllWindows() 
