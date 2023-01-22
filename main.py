2710df2e90ce8de12f0f7910d8615983edf22f02
Satyam-Satyarthi <107866501+Satyam-Satyarthi@users.noreply.github.com>
2023-01-22 21:31:19 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,15 +7,22 @@ camera = VideoCapture(0)
 
 while True:
     _, frame = camera.read()
+    #Output of Normal Camera
+    
+    cv.imshow('Camera',frame)
 
-    #cv.imshow('Camera',frame)
-
+    #Output from Laplacian Filter
+    
     laplacian = cv.Laplacian(frame , cv.CV_64F)
     #laplacian = np.unit8(laplacian)
-    # cv.imshow('Laplacian',laplacian)
-
+    cv.imshow('Laplacian',laplacian)
+    
+    #Output from Canny Filter
+    
     edges = cv.Canny(frame , 200 , 200)
     cv.imshow('Canny', edges)
+    
+    #Output from Sobel Filter
 
     frame = cv.cvtColor(frame,cv.COLOR_BGR2GRAY).astype(float)
     edge_x = cv.Sobel(frame , cv.CV_64F,1,0,ksize=3)
